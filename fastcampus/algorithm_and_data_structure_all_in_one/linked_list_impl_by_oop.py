0526a4d654ebd3d60b7d4b2e85a0006bee59d582
Redonearth <wona23@gmail.com>
2020-11-13 22:39:11 +0900

    링크드 리스트 설명 추가

--- fastcampus/algorithm_and_data_structure_all_in_one/linked_list_impl_by_oop.py
+++ fastcampus/algorithm_and_data_structure_all_in_one/linked_list_impl_by_oop.py
@@ -1,3 +1,6 @@
+# Node 구현
+# 파이썬에서 일반적으로 링크드 리스트 구현 시, 파이썬 클래스를 활용
+# 파이썬 객체지향 문법 이해 필요
 class Node:
     def __init(self, data, next=None):
         self.data = data
@@ -8,6 +11,7 @@ class NodeMgmt:
     def __init__(self, data):
         self.head = Node(data)
 
+    # 링크드 리스트의 복잡한 기능1 (링크드 리스트 사이에 데이터 추가)
     def add(self, data):
         if self.head == '':
             self.head = Node(data)
@@ -17,6 +21,7 @@ class NodeMgmt:
                 node = node.next
             node.next = Node(data)
 
+    # 링크드 리스트의 복잡한 기능2 (특정 노드를 삭제)
     def delete(self):
         if self.head == '':
             print("해당 값을 가진 노드가 없습니다.")
