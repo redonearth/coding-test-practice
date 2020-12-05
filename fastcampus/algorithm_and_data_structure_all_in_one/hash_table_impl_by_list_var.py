fd995231077d6600eb87eb9346f1ac0210ec5280
Redonearth <wona23@gmail.com>
2020-12-05 16:11:55 +0900

    리스트 변수로 해시 테이블 구현

---
+++ fastcampus/algorithm_and_data_structure_all_in_one/hash_table_impl_by_list_var.py
@@ -0,0 +1,26 @@
+hash_table = list([0 for i in range(8)])
+
+
+def get_key(data):
+    return hash(data)
+
+
+def hash_function(key):
+    return key % 8
+
+
+def save_data(data, value):
+    hash_address = hash_function(get_key(data))
+    hash_table[hash_address] = value
+
+
+def read_data(data):
+    hash_address = hash_function(get_key(data))
+    return hash_table[hash_address]
+
+
+save_data('Redonearth', '01010001000')
+save_data('Koongdori', '01020002000')
+
+print(read_data('Redonearth'))
+print(hash_table)
