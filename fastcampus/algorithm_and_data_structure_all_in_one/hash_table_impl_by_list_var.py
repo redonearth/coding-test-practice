678eb37b687f7d55e5715b09ed896cb626c8cdd4
Redonearth <wona23@gmail.com>
2020-12-06 20:14:09 +0900

    해시 테이블 충돌(Collision) 해결 - Chaining 기법

--- fastcampus/algorithm_and_data_structure_all_in_one/hash_table_impl_by_list_var.py
+++ fastcampus/algorithm_and_data_structure_all_in_one/hash_table_impl_by_list_var.py
@@ -10,17 +10,36 @@ def hash_function(key):
 
 
 def save_data(data, value):
-    hash_address = hash_function(get_key(data))
-    hash_table[hash_address] = value
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key:
+                hash_table[hash_address][index][1] = value
+                return
+            hash_table[hash_address].append([index_key, value])
+    else:
+        hash_table[hash_address] = [[index_key, value]]
 
 
 def read_data(data):
-    hash_address = hash_function(get_key(data))
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key:
+                return hash_table[hash_address][index][1]
+        return None
+    else:
+        return None
     return hash_table[hash_address]
 
 
 save_data('Redonearth', '01010001000')
 save_data('Koongdori', '01020002000')
+save_data('Dave', '01030003000')
+save_data('Dd', '01040004000')
 
 print(read_data('Redonearth'))
+print(read_data('Dd'))
 print(hash_table)
