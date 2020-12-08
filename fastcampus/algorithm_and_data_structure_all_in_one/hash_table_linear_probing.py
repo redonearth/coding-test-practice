634c2392566b7e174b82a0ed7fa139bffbd61069
Redonearth <wona23@gmail.com>
2020-12-09 00:01:37 +0900

    Linear Probing 기법 코드 개선

--- fastcampus/algorithm_and_data_structure_all_in_one/hash_table_linear_probing.py
+++ fastcampus/algorithm_and_data_structure_all_in_one/hash_table_linear_probing.py
@@ -3,11 +3,16 @@
 # 충돌이 발생하면, 해당 hash address의 다음 address부터 가장 처음으로 나오는 빈 공간에 저장하는 기법
 # 저장 공간 활용도를 높이기 위한 기법
 
+import hashlib
+
 hash_table = list([0 for i in range(8)])
 
 
 def get_key(data):
-    return hash(data)
+    hash_object = hashlib.sha256()
+    hash_object.update(data.encode())
+    hex_dig = hash_object.hexdigest()
+    return int(hex_dig, 16)  # 16진수의 문자열을 10진수로 변환
 
 
 def hash_function(key):
@@ -32,12 +37,21 @@ def save_data(data, value):
 def read_data(data):
     index_key = get_key(data)
     hash_address = hash_function(index_key)
+
     if hash_table[hash_address] != 0:
-        for index in range(hash_table[hash_address]):
+        for index in range(hash_address, len(hash_table)):
             if hash_table[index] == 0:
                 return None
-            elif hash_table[index] == index_key:
+            elif hash_table[index][0] == index_key:
                 return hash_table[index][1]
-        return None
     else:
         return None
+
+
+save_data('Dave', 'Chief')
+save_data('Chris', 'Soldier')
+save_data('Redonearth', 'Programmer')
+
+print(read_data('Dave'))
+print(read_data('Chris'))
+print(read_data('Redonearth'))
