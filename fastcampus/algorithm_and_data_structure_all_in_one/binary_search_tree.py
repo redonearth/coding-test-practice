b823fe97f828dca1c2e45a881f255413e92aa8e9
Redonearth <wona23@gmail.com>
2020-12-11 22:28:19 +0900

    이진 탐색 트리 구현

---
+++ fastcampus/algorithm_and_data_structure_all_in_one/binary_search_tree.py
@@ -0,0 +1,33 @@
+# 이진 탐색 트리(Binary Search Tree) 구현
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head  # Root Node
+
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+
+
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)
