daee3b545ebb95f283564f9de98372ef86b7e602
Redonearth <wona23@gmail.com>
2020-12-12 19:51:49 +0900

    탐색 코드 추가

--- fastcampus/algorithm_and_data_structure_all_in_one/binary_search_tree.py
+++ fastcampus/algorithm_and_data_structure_all_in_one/binary_search_tree.py
@@ -27,7 +27,24 @@ class NodeMgmt:
                     self.current_node.right = Node(value)
                     break
 
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+        return False
+
 
 head = Node(1)
 BST = NodeMgmt(head)
-BST.insert(2)
+BST.insert(0)
+BST.insert(3)
+BST.insert(6)
+BST.insert(7)
+BST.insert(9)
+
+print(BST.search(-1))
