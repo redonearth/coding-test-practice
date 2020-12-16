77686c8c604e67173218dff0c99f1cceb6855e0a
Redonearth <wona23@gmail.com>
2020-12-16 23:19:32 +0900

    삭제할 Node의 Case 2가지 작성

--- fastcampus/algorithm_and_data_structure_all_in_one/binary_search_tree.py
+++ fastcampus/algorithm_and_data_structure_all_in_one/binary_search_tree.py
@@ -38,6 +38,47 @@ class NodeMgmt:
                 self.current_node = self.current_node.right
         return False
 
+    def delete(self, value):
+        searched = False
+        self.current_node = self.head  # 삭제할 대상 Node
+        self.parent = self.head  # 삭제할 Node의 parent도 가지고 있어야 한다.
+        while self.current_node:
+            if self.current_node.value == value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent = self.current_node
+                self.current_node = self.current_node.right
+
+        if searched == False:
+            return False
+
+        # 이후부터 Case를 나누어 코드 작성
+
+        # Case 1. 삭제할 Node가 Leaf Node인 경우
+        # self.current_node가 삭제할 Node, self.parent는 삭제할 Node의 Parent Node인 상태
+        if self.current_node.left == None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = None
+            else:
+                self.parent.right = None
+            del self.current_node
+
+        # Case 2. 삭제할 Node가 Child Node를 한 개 가지고 있을 경우
+        if self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.left
+            else:
+                self.parent.right = self.current_node.left
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.right
+            else:
+                self.parent.right = self.current_node.right
+
 
 head = Node(1)
 BST = NodeMgmt(head)
